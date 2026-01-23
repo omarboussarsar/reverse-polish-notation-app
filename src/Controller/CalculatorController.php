249ad469d6db49d89c714ae091a517a641898963
Omar BOUSSARSAR <boussarsar.omar@gmail.com>
2026-01-23 18:46:13 +0100

    :hammer: Use AJAX

--- src/Controller/CalculatorController.php
+++ src/Controller/CalculatorController.php
@@ -7,28 +7,37 @@ namespace App\Controller;
 use App\Service\ReversePolishNotation;
 use InvalidArgumentException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class CalculatorController extends AbstractController
 {
-    #[Route('/', name: 'calculator', methods: ['GET', 'POST'])]
-    public function index(Request $request, ReversePolishNotation $calculator): Response
+    #[Route('/', name: 'calculator_form', methods: ['GET'])]
+    public function form(): Response
+    {
+        return $this->render('calculator/index.html.twig', [
+            'expression' => '',
+            'result' => null,
+            'error' => null,
+        ]);
+    }
+
+    #[Route('/evaluate', name: 'calculator_evaluate', methods: ['POST'])]
+    public function evaluate(Request $request, ReversePolishNotation $calculator): JsonResponse
     {
         $expression = trim((string) $request->request->get('expression', ''));
         $result = null;
         $error = null;
 
-        if ($request->isMethod('POST')) {
-            try {
-                $result = $calculator->evaluate($expression);
-            } catch (InvalidArgumentException $exception) {
-                $error = $exception->getMessage();
-            }
+        try {
+            $result = $calculator->evaluate($expression);
+        } catch (InvalidArgumentException $exception) {
+            $error = $exception->getMessage();
         }
 
-        return $this->render('calculator/index.html.twig', [
+        return $this->json([
             'expression' => $expression,
             'result' => $result,
             'error' => $error,
